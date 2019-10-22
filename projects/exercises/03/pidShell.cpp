b48de2165e4997325fb0b61914be17082ab9b73c
Zolkin1 <zach.olkin@gmail.com>
2019-10-21 20:05:47 -0400

    adding some more basic starting code

--- projects/exercises/03/pidShell.cpp
+++ projects/exercises/03/pidShell.cpp
@@ -1,1 +1,13 @@
 // Implement the two functions in the pid.h file
+
+// Constructor
+PID::PID(float p, float i, float d, float dt, float saturation, float antiwindup)
+{
+
+}
+
+// PID calculation
+float PID::update(float target, float current)
+{
+
+}
